48169e2a556a7acd2a057a90921fe4ee692e0576
Maaike Konijnendijk <maaike.konijnendijk@hotmail.com>
2022-08-29 19:07:21 +0200

    Updates

---
+++ precache-manifest.2267799f224e896687992e3bfc3d8c39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ba79f0f4f474c0122776",
+    "url": "/webdev-portfolio/static/css/main.38195da7.chunk.css"
+  },
+  {
+    "revision": "ba79f0f4f474c0122776",
+    "url": "/webdev-portfolio/static/js/main.81d7baa1.chunk.js"
+  },
+  {
+    "revision": "0a04550277ecb60809d1",
+    "url": "/webdev-portfolio/static/js/runtime~main.c5aba466.js"
+  },
+  {
+    "revision": "4bcc576efa8734532f5c",
+    "url": "/webdev-portfolio/static/js/2.59b3a046.chunk.js"
+  },
+  {
+    "revision": "2ded26446bc94b81347a",
+    "url": "/webdev-portfolio/static/js/3.398cb210.chunk.js"
+  },
+  {
+    "revision": "e3634c9eb1b3e01f287deafcbd2d1100",
+    "url": "/webdev-portfolio/index.html"
+  }
+];
